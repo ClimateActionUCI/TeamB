156f940a29ab83b6c3fb505cbaaa130e0edfeff2
Gebbran <dbacilla@uci.edu>
2017-03-22 09:37:11 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -18,9 +18,29 @@ shinyServer(function(input,output) {
                 #matB5 = matrix(rnorm(9),3,3)
   
                 Fusion = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Fusion.tif')
-
-                matA_v = list(Fusion)
-                matB_v = list(Fusion)
+                
+                A1B_2025 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2025_A1B.tif')
+                A1B_2050 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2050_A1B.tif')
+                A1B_2075 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2075_A1B.tif')
+                A1B_2100 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2100_A1B.tif')
+                
+                A2_2025 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2025_A2.tif')
+                A2_2050 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2050_A2.tif')
+                A2_2075 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2075_A2.tif')
+                A2_2100 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2100_A2.tif')
+                
+                B1_2025 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2025_B1.tif')
+                B1_2050 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2050_B1.tif')
+                B1_2075 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2075_B1.tif')
+                B1_2100 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2100_B1.tif')
+                
+                B2_2025 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2025_B2.tif')
+                B2_2050 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2050_B2.tif')
+                B2_2075 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2075_B2.tif')
+                B2_2100 = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Land_Use_2100_B2.tif')
+                
+                matA_v = list(Fusion, B1_2025, B1_2050, B1_2075, B1_2100)
+                matB_v = list(Fusion, A2_2025, A2_2050, A2_2075, A2_2100)
 
                 #matA_v = list(matA1, matA2, matA3, matA4, matA5)
                 #matB_v = list(matB1, matB2, matB3, matB4, matB5)
@@ -40,7 +60,7 @@ shinyServer(function(input,output) {
                   print(input$scenario)
                   print(input$scenario)
                   mat_row = as.numeric(input$scenario)
-                  mat_col = as.numeric(input$year)
+                  mat_col = 1 + (as.numeric(input$year) - 2000) / 25
                   temp = all_matrix[mat_row, mat_col][[1]]
                   raster::plot(temp)
                   })
--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ shinyUI(pageWithSidebar(
                         headerPanel("Land Use Change Under Two Different Emissions Scenarios"),
                         sidebarPanel(
                                      sliderInput("year", "Year:",
-                                                 min = 1, max = 1, step = 1, value = 1),
+                                                 min = 2000, max = 2100, step = 25, value = 2000),
                                      selectInput(inputId = "scenario",
                                                  label = "Emissions Scenario:",
                                                  choices = c(1,2),
