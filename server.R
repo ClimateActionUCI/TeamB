668dbc030339a0708aa60b6c83dde87090ad5f67
Gebbran <dbacilla@uci.edu>
2017-03-21 17:05:36 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -1,30 +1,48 @@
 library(shiny)
 library(ggplot2)
 library(ggmap)
+library(raster)
+
+#Land <- raster("Fusion")
 
 shinyServer(function(input,output) {
-                matA1 = matrix(rnorm(9),3,3)
-                matA2 = matrix(rnorm(9),3,3)
-                matA3 = matrix(rnorm(9),3,3)
-                matA4 = matrix(rnorm(9),3,3)
-                matA5 = matrix(rnorm(9),3,3)
-                matB1 = matrix(rnorm(9),3,3)
-                matB2 = matrix(rnorm(9),3,3)
-                matB3 = matrix(rnorm(9),3,3)
-                matB4 = matrix(rnorm(9),3,3)
-                matB5 = matrix(rnorm(9),3,3)
+                #matA1 = matrix(rnorm(9),3,3)
+                #matA2 = matrix(rnorm(9),3,3)
+                #matA3 = matrix(rnorm(9),3,3)
+                #matA4 = matrix(rnorm(9),3,3)
+                #matA5 = matrix(rnorm(9),3,3)
+                #matB1 = matrix(rnorm(9),3,3)
+                #matB2 = matrix(rnorm(9),3,3)
+                #matB3 = matrix(rnorm(9),3,3)
+                #matB4 = matrix(rnorm(9),3,3)
+                #matB5 = matrix(rnorm(9),3,3)
+  
+                Fusion = raster('D:/Estudos/UCI PhD/CATP 2017/Project/TeamB-master/Fusion.tif')
 
-                matA_v = list(matA1, matA2, matA3, matA4, matA5)
-                matB_v = list(matB1, matB2, matB3, matB4, matB5)
+                matA_v = list(Fusion)
+                matB_v = list(Fusion)
 
+                #matA_v = list(matA1, matA2, matA3, matA4, matA5)
+                #matB_v = list(matB1, matB2, matB3, matB4, matB5)
+  
                 all_matrix = rbind(matA_v, matB_v)
                 
-                select_matrix = reactive({
+                #select_matrix = reactive({
+                #  print(input$scenario)
+                #  mat_row = as.numeric(input$scenario)
+                #  mat_col = as.numeric(input$year)
+                #  all_matrix[mat_row, mat_col]
+                #})
+        
+      
+      #output$values <- renderPlot({plot(select_matrix()}), width = "100%", height = "400px")
+                output$values <- renderPlot({
+                  print(input$scenario)
                   print(input$scenario)
                   mat_row = as.numeric(input$scenario)
                   mat_col = as.numeric(input$year)
-                  all_matrix[mat_row, mat_col]
-                })
-      
-      output$values <- renderPrint(select_matrix())
+                  temp = all_matrix[mat_row, mat_col][[1]]
+                  raster::plot(temp)
+                  })
+                #output$values <- plot(select_matrix())
 })      
--- ui.R
+++ ui.R
@@ -4,10 +4,11 @@ shinyUI(pageWithSidebar(
                         headerPanel("Land Use Change Under Two Different Emissions Scenarios"),
                         sidebarPanel(
                                      sliderInput("year", "Year:",
-                                                 min = 1, max = 5, step = 1, value = 1),
+                                                 min = 1, max = 1, step = 1, value = 1),
                                      selectInput(inputId = "scenario",
                                                  label = "Emissions Scenario:",
                                                  choices = c(1,2),
                                                  selected = 1)),
-                        mainPanel(verbatimTextOutput("values"))
+                        #mainPanel(verbatimTextOutput("values"))
+                        mainPanel(plotOutput("values", width = "100%", height = "800px"))
                                      ))                                                 
